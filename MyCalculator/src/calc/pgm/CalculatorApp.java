d5f8bb1f4d877d705d87fec1ff57e87f97025e38
monik <monik@Monika>
2023-08-30 17:17:16 +0530

    First commit

---
+++ MyCalculator/src/calc/pgm/CalculatorApp.java
@@ -0,0 +1,84 @@
+package calc.pgm;
+import java.util.Scanner;
+
+class Mycalc {
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    int sub(int a, int b) {
+        return a - b;
+    }
+
+    int mul(int a, int b) {
+        return a * b;
+    }
+
+    double div(int a, int b) {
+        if (b != 0) {
+            return (double) a / b;
+        } else {
+            throw new ArithmeticException("Divide by zero error!!!");
+        }
+    }
+}
+
+public class CalculatorApp {
+
+	public static void main(String[] args) {
+		
+		 Scanner sc = new Scanner(System.in);
+	        System.out.println("Welcome to Simple Calculator");
+	        System.out.println("Simple Menu:");
+	        System.out.println("1. Addition");
+	        System.out.println("2. Subtraction");
+	        System.out.println("3. Multiplication");
+	        System.out.println("4. Division");
+	        System.out.print("Please Select an Operation (1/2/3/4): ");
+	        int choice = sc.nextInt();
+
+	        if (choice < 1 || choice > 4) {
+	            System.out.println("Invalid choice");
+	            sc.close();
+	            return;
+	        }
+
+	        System.out.print("Enter first Number: ");
+	        int a = sc.nextInt();
+	        System.out.print("Enter second Number: ");
+	        int b = sc.nextInt();
+	        double result = 0;
+
+	        Mycalc calculator = new Mycalc();
+
+	        if (choice == 1) {
+	            result = calculator.add(a, b);
+	            System.out.println("The sum is " + result);
+	        } else if (choice == 2) {
+	            result = calculator.sub(a, b);
+	            System.out.println("The difference is " + result);
+	        } else if (choice == 3) {
+	            result = calculator.mul(a, b);
+	            System.out.println("The product is " + result);
+	        } else if (choice == 4) {
+	            result = calculator.div(a, b);
+	            System.out.println("The quotient is " + result);
+	        }
+
+	        System.out.print("Do you want to perform another calculation? (yes/no): ");
+	        String continueChoice = sc.next();
+
+	        if (continueChoice.equalsIgnoreCase("yes")) {
+	            main(args);
+	        } else {
+	            System.out.println("Thank you!");
+	        }
+
+	        sc.close();
+
+	}
+
+}
+
+
+
